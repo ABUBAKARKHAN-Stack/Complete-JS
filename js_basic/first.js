5feec284d57352f50f77fce923722a82a983093c
Abubakar-Aijaz <147841065+ABUBAKARKHAN-Stack@users.noreply.github.com>
2023-12-01 12:50:50 +0000

    first_tut

---
+++ js_basic/first.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+ /* NOT to Use these Types Of Conversion
+// console.log("3" > 1);
+// console.log("04" > 3);
+
+
+//  console.log(null > 0);
+//  console.log(null >= 0);
+//  console.log(null == 0);
+
+//  console.log(undefined > 0);
+//  console.log(undefined >= 0);
+//  console.log(undefined == 0);
+
+*/
+// ===
+// console.log("3" === 0);
+
