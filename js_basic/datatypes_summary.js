b00a5e6e090b55ef3b9dcad55db8a111377beb32
Abubakar-Aijaz <147841065+ABUBAKARKHAN-Stack@users.noreply.github.com>
2023-12-01 13:24:21 +0000

    Notes OF Data Types

---
+++ js_basic/datatypes_summary.js
@@ -0,0 +1,71 @@
+// Primitive Data Types //
+// There are 7 TYPES //
+/* 
+    1: String
+    2: Numbers
+    3: Boolean
+    4: BigInt
+    5: Null
+    6: Undefinied
+    7: Symbol
+*/ 
+   // 1: Strings
+//    const name = "Abu-bakar"
+
+
+   // 2: Numbers
+//    const score = 100
+
+   // 3: Boolean
+//    const isLogedIn = True
+
+
+// 4: BigIng
+    //   const bigNum = 999999999999n
+    //   console.log(typeof bigNum);
+
+
+   // 5: Null 
+//    const tempOutside = null
+
+
+   // 6: Undefinied
+    // let emailId;
+
+
+    // 7: Symbol 
+    // let id = Symbol('123')
+    // let userId = Symbol('123')
+    // console.log(id === userId);
+
+
+
+
+
+ 
+// Referances (NON-Primitive DT) //
+/* 
+     1: Array
+     2: Objects
+     3: Functions
+*/
+
+ // 1: Array
+//  const hero = ["batman" , "spiderman" , "Superman"];
+//  console.log(typeof hero)
+
+ // 2: Object
+//   let obj ={
+//     name : "Abubakar",
+//     Age : "16",
+//     Gender : "Male"
+//   }
+//   console.log(typeof obj);
+
+ // 3: Functions
+ /* const myFunc = function()
+ {
+      console.log("Hello world");
+ } 
+ */
+//                      NOTE: Data Type Of Non-Primitive (DT) Always Object                        //
