fd371bba78829693b28d0cca354e3ec4f120dd15
Abubakar-Aijaz <147841065+ABUBAKARKHAN-Stack@users.noreply.github.com>
2023-12-02 12:40:49 +0000

    Strings Notes / Practice

---
+++ js_basic/strings.js
@@ -0,0 +1,78 @@
+// const name = "abubakar "
+// const age = 16
+// const height = "6.2cm"
+
+// // console.log(name +  age + " 6.2cm");
+// console.log(`Hello My Name is ${name} and my Age is ${age} and my height is ${height}`);
+// Output = Hello My Name is abubakar  and my Age is 16 and my height is 6.2cm //
+
+// Here's New Way To declere string //
+// const gameName = new String('IGI-1')
+// To Print Key Value //
+// console.log(gameName[0]); // 
+// Output = I // >= Bcz Index-Start From 0 to n .. //
+
+// To Find Length //
+// console.log(gameName.length);
+// Output = 5 >= Bcz Length = 5 ..//
+
+// To Print Prototype //
+// console.log(gameName.__proto__); //
+
+ // To Print Uppercase letters //
+//  const city = new String('karachi')
+//  console.log(city.toUpperCase());
+ // Output = KARACHI //
+
+ // To Print LowerCase letters //
+//  const Console = new String('X-BOX')
+//  console.log(Console.toLowerCase());
+//  Output = x-box 
+ 
+// TO Find The Position Of Character In Index // 
+// const position = "first"
+// console.log(position.charAt(3));
+// Output = s => BCZ s On third position
+
+// // TO Find The Position Of Character // 
+// const position = "first"
+// console.log(position.indexOf('t'));
+// // Output = 4 => BCZ t On Fourth position
+
+// Slice of Methood of String //
+// const password = "digit"
+// console.log(password.slice(1, 4));
+// Output = igi //
+  
+// To replace other string
+// const place = "paris"
+// console.log(place.replace('paris', 'newyork'));
+// Output = newyork
+
+// To Remove Unwanted Spaces // 
+// const space = "    google.   "
+// console.log(space.trim());
+// Output = google.
+
+// To Find The Keyword //
+// const word = "chemistry-physics"
+// console.log(word.includes('physics'));
+// Output = True .
+// It always gives value in Boolean (true/false)
+
+// const list = {
+//     fooditem : "just-Tomato" ,
+//     drink : "pepsi_7up"
+// }
+// console.log(list.drink.split("_"));
+// Output = [ 'pepsi', '7up' ]
+
+ // To Join words //
+// const big = "bold"
+// console.log(big.concat("/thin"));
+// Output = bold/thin
+
+
+
+
+
